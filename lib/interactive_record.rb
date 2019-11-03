06c70faaca181e1c6ba6b5f56bb1f76dcf52718e
Kierra M <k.merriman32@hotmail.com>
2019-11-03 02:37:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ def self.find_by_name(name)
 end
 
 def self.find_by (hash)
-sql = "SELECT * FROM #{self.table_name} WHERE #{hash.key"
+sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0]"
 
 end
   
