1f9a30770d0d213117cf836e8ee5f53fbd962484
Kierra M <k.merriman32@hotmail.com>
2019-11-03 02:22:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,6 +34,7 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}") [0][0]
   end
       
-  
+  def table_name_for_insert
+    
   
 end
