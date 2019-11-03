87e937c5bae43b258b6214cc1a22623f78321564
Kierra M <k.merriman32@hotmail.com>
2019-11-03 02:19:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,6 +30,7 @@ class InteractiveRecord
     
     sql = "INSERT INTO #{table_name_for_insert} 
     (#{col_names_for_insert} VALUES #{values_for_insert}" 
+    DB[:conn].execute(sql)
       
   
   
