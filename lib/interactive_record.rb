fe5a26237938db2c675af4aa241997dca4ec5309
Kierra M <k.merriman32@hotmail.com>
2019-11-03 02:25:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,7 +39,7 @@ class InteractiveRecord
   end
   
   def col_names_for_insert
-    self.class.col_names.delete_if {|col| col == "id"}.join(" , ")
+    self.class.column_names.delete_if {|col| col == "id"}.join(" , ")
   end
 
 
